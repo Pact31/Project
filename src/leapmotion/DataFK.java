8f1fb7b415c6ebb9617586d41a968752f1362416
Hassen KHROUF <hassen.khrouf@telecom-paristech.fr>
2014-03-03 13:47:09 +0100

    ajout des deux classes DATAFK et Test

---
+++ src/leapmotion/DataFK.java
@@ -0,0 +1,13 @@
+package leapmotion;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Hashtable;
+import com.leapmotion.leap.*;
+public class DataFK {
+	public enum Key {
+	      PDJ, KVZ, SR, BNUI, MTF, ICHGNW, G, YNG; //Correspond aux différentes "consonnes" codées par les clefs.
+	}
+	   Hashtable<Frame, Integer> table
+	     = new Hashtable<Frame, Integer>();
+
+}
