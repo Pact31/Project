6aa2637a78a85566d046e3b1215f2ff219850f96
ABBES Ahmad <ahmad.abbes@telecom-paristech.fr>
2014-03-03 10:33:47 +0100

    Ajout de l'interface SoundInterface et de sa classe associée

---
+++ src/syntheseparole/SoundInterface.java
@@ -0,0 +1,12 @@
+
+public interface SoundInterface {
+	
+	
+	/* First simple method to concatenate two songs */
+	public Sound cat1 (Sound snd);
+
+	/* Second method to concatenate two songs which uses interpolation */
+	public Sound cat2 (Sound snd);
+	
+
+}
---
+++ src/syntheseparole/main.java
@@ -0,0 +1,19 @@
+import java.io.File;
+
+
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		//InterfacePlayer player = new InterfacePlayer("data/pain.wav");
+        //player.setVisible(true);
+        File file = new File("data/pa1.wav");
+        System.out.println(file.exists());
+        Thread song = new Sound("data/po(beau).wav");
+        song.run();
+	}
+
+}
