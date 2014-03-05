b797f8d037c758ef9becc918071408b775b07e35
xgan <xiangyang.gan@gmail.com>
2014-03-05 23:24:01 +0100

    pret à merge

--- src/main/MainTest.java
+++ src/main/MainTest.java
@@ -18,19 +18,19 @@ public class MainTest {
 		/*
 		 * the principal window
 		 */
-		/*DrawingAppModel		model				=	new	DrawingAppModel();
+		DrawingAppModel		model				=	new	DrawingAppModel();
 		HandSpeakController handSpeakController = 	new HandSpeakController(model);
 		DrawingApp 			drawingApp 			= 	new DrawingApp(handSpeakController);
-		*/
-		MyApprentissage app = new MyApprentissage();
 		
-		app.saveBank("123");
+		//MyApprentissage app = new MyApprentissage();
+		
+		//app.saveBank("123");
 		/*
 		 * use second thread for showing the detection of Leap Motion
 		 */
-		/*ThreadLeapMotion threadLeapMotion 		=	new	ThreadLeapMotion(drawingApp);
+		ThreadLeapMotion threadLeapMotion 		=	new	ThreadLeapMotion(drawingApp);
 		threadLeapMotion.start();
-		*/
+		
 	}
 }
 
