97e312faabed1e79653ec4a56125ea42747d8f46
Philippe Boneff <philippeboneff@outlook.com>
2014-03-07 16:14:39 +0100

    Version BETA avec la classe LeapData

---
+++ src/LeapTS/LeapData.java
@@ -0,0 +1,19 @@
+package LeapTS;
+
+import com.leapmotion.leap.Frame;
+
+import classification.Cible;
+
+public class LeapData extends FrameTS{
+
+	private Cible cible;
+	
+	public LeapData(Frame frame, Cible cible){
+		super(frame);
+		this.cible = cible;
+	}
+	
+	public Cible getCible(){
+		return this.cible;
+	}
+}
