ede0c24e905bd671b362047a160e825058786b1b
Luka Music <luka-music@c124-03.enst.fr>
2014-03-08 16:32:12 +0100

    add ignore .project

--- src/Main.java
+++ src/Main.java
@@ -19,14 +19,13 @@ public class Main {
 		
 
 		if(args.length!=3){
-			System.out.print("utilisation : classifier num, T, K");
+			System.out.println("utilisation : T, k");
 		}
 		
-		int classifierNum = Integer.valueOf(args[0]).intValue();//classifier num = 1 lance adaboost, le reste lance kppv
 		
-		int T = Integer.valueOf(args[1]).intValue();//nombre de tour d'apprentissage pour adaboost
+		int T = Integer.valueOf(args[0]).intValue();//nombre de tour d'apprentissage pour adaboost
 		
-		int k = Integer.valueOf(args[2]).intValue();//nombre de voisins pour kppv
+		int k = Integer.valueOf(args[1]).intValue();//nombre de voisins pour kppv
 		
 		LeapDataBase leapDataBase = new LeapDataBase();//initialisation de la base de donnée
 		
@@ -38,17 +37,11 @@ public class Main {
 		
 		System.out.println("Banque d'apprentissage initialisée");
 		
-		if(classifierNum == 1){
-			Adaboost a=new Adaboost(banque, T);
-			System.out.print("AdaBoost initialisé");
+		Adaboost adaboost =new Adaboost(banque, T);
+		System.out.print("AdaBoost initialisé");
 			
-		}
-		
-		else{
-			Kppv a = new Kppv(banque, k);
-			System.out.print("Kppv initialisé");
-			
-		}
+		Kppv kppv = new Kppv(banque, k);
+		System.out.print("Kppv initialisé");
 
 		DrawingAppModel		model				=	new	DrawingAppModel();
 		HandSpeakController handSpeakController = 	new HandSpeakController(model);
