3aff8d2093f454467c4cbf5f2c8f25ce3faf4d5d
Philippe Boneff <philippeboneff@outlook.com>
2014-03-08 17:03:19 +0100

    Modification de la methode put de LeapDataBase : creation d'un constructeur LeapData

--- src/LeapTS/LeapData.java
+++ src/LeapTS/LeapData.java
@@ -1,5 +1,7 @@
 package LeapTS;
 
+import leapmotion.LetterException;
+
 import com.leapmotion.leap.Frame;
 
 import classif.classification.Cible;
@@ -17,6 +19,39 @@ public class LeapData extends FrameTS{
 	public LeapData(Frame frame, Cible cible){
 		super(frame);
 		this.cible = cible;
+	}
+	
+		public LeapData( Frame frame,char inChar) throws IllegalArgumentException, NullPointerException, LetterException{
+		super(frame);
+		switch(inChar){
+		case 'a' : 
+			this.cible = Cible.PDJ;
+			break;
+		case 'z':
+			this.cible = Cible.KVZ;
+			break;
+		case 'e':
+			this.cible = Cible.SR;
+			break;
+		case 'r':
+			this.cible = Cible.G;
+			break;
+		case 't':
+			this.cible = Cible.ICHGNW;
+			break;
+		case 'y': 			
+			this.cible = Cible.MTF;
+			break;
+		case 'u': 
+			this.cible = Cible.YNG;
+			break;
+		case 'i':
+			this.cible = Cible.BNUI;
+			break;
+		default: 
+			throw (new LetterException(inChar));
+			
+		}
 	}
 	
 	
