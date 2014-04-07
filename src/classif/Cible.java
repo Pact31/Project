38c05e6dab29c79238f0d313f4390618a49b1705
Philippe Boneff <philippeboneff@outlook.com>
2014-04-07 16:03:38 +0200

    Modification de Cible.java pour tenir compte de la position de la main. Il y a maintenant 40 cibles.

--- src/classif/Cible.java
+++ src/classif/Cible.java
@@ -10,5 +10,12 @@ package classif;
 
 
 public enum Cible {
-  	PDJ, KVZ, SR, BNUI, MTF, ICHGNW, G, YNG; //Correspond aux diff�rentes "consonnes" cod�es par les clefs.
+  	PDJ_G, PDJ_M, PDJ_B, PDJ_C, PDJ_P,
+  	KVZ_G, KVZ_M, KVA_B, KVZ_C, KVZ_P, 
+  	SR_G, SR_M, SR_B, SR_C, SR_P, 
+  	BNUI_G, BNUI_M, BNUI_B, BNUI_C, BNUI_P,
+  	MTF_G, MTF_M, MTF_B, MTF_C, MTF_P,
+  	ICHGNW_G, ICHGNW_M, ICHGNW_B, ICHGNW_C, ICHGNW_P,
+  	G_G, G_M, G_B, G_C, G_P,
+  	YNG_G, YNG_M, YNG_B, YNG_C, YNG_P; //Correspond aux diff�rentes "consonnes" cod�es par les clefs.
 }
